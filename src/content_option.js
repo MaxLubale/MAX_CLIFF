8dee017fbe6c35f49c1fb700fe7c9c3cd248fd2f
Maxwell <lubalecliff@gmail.com>
2024-07-15 23:21:22 +0300

    Complete v1

---
+++ src/content_option.js
@@ -0,0 +1,214 @@
+const logotext = "M.C";
+const meta = {
+    title: "Max Cliff",
+    description: "I’m Max Cliff, a problem solver _ Tech Mastermind,based in Nairobi, Kenya",
+};
+
+const introdata = {
+    title: "I’m Max Cliff",
+    animated: {
+        first: "I love coding",
+        second: "I create cool websites",
+        third: "I develop mobile apps",
+        fourth: "I also do modern designs and art",
+        fifth: "I am phenomenal😎"
+
+    },
+    description: "I’m Max Cliff, a problem solver _ Tech Mastermind,based in Nairobi",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Hi! am MAX. A passionate software engineer with relevant experience of 3 years. Always looking  opportunities to grow in my career,be a team player and collaborate with others. I'd love the opportunity to know more about your needs and how we will come up with a creative solution whether as a fellow dev or client. Hope I'll be considered. Looking forward to working with you.",
+};
+const worktimeline = [{
+        jobtitle: "CHIEF OF ENGINEERING 🤓",
+        where: "Nirmata Wealth Inc Dubai(NEO TRILLI AO)",
+        date: "Current Role",
+    },
+    {
+        jobtitle: "Software Developer",
+        where: "Intasend",
+        date: "2024(Contract Role)",
+    },
+    {
+        jobtitle: "Software Engineer",
+        where: "St Bakhita Mission Hospital Facilty TRust",
+        date: "2023-2024(Contract Role)",
+    },
+    {
+        jobtitle: "SAAS Software Engineer",
+        where: "Simplyinfo.org(UK)",
+        date: "2022",
+    },
+    {
+        jobtitle: "Freelancer",
+        where: "Everywhere 🤭",
+        date: "Current Role",
+    },
+    
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+    {
+        name: "PHP",
+        value: 60,
+    }, 
+    {
+        name: "NodeJs",
+        value: 80,
+    },
+    {
+        name: "ExpressJs",
+        value: 60,
+    },
+    {
+        name: "C++",
+        value: 50,
+    },
+    {
+        name: "C#",
+        value: 50,
+    },
+];
+
+const services = [{
+    
+        title: "UI & UX Design",
+        description: "Crafting intuitive and aesthetically pleasing interfaces that offer seamless user experiences, ensuring your digital presence captivates and engages."
+    },
+    {
+        title: "Mobile Apps",
+        description: "Developing innovative and user-friendly mobile applications tailored to your business needs, delivering powerful performance right at your fingertips."
+    },
+    {
+        title: "WordPress Design",
+        description: "Creating customized and responsive WordPress websites that reflect your brand’s identity, combining functionality with elegant design."
+    },
+    {
+        title: "E-commerce Websites",
+        description: "Building robust and scalable e-commerce platforms that enhance online shopping experiences, driving conversions and fostering customer loyalty."
+    },
+    {
+        title: "Business Management System Solutions",
+        description: "Implementing comprehensive business management systems to streamline operations, enhance productivity, and enable data-driven decision making.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://cdn.builder.io/api/v1/image/assets/TEMP/23af0e2870273aeaf2ea604095d5eb26edddfdb294309077164fbcbb6f4a6570?apiKey=07b7466444764966b3e325d12cdd6280&",
+        description: "Trillion dollar investment suite campaign for incestors.",
+        link: "https://trilliao.com/",
+    },
+    {
+        img: "https://cdn.builder.io/api/v1/image/assets/TEMP/f02f7d70016b90b8181d947d9707bc35070c78f02266d6efa422321173bc70e8?apiKey=07b7466444764966b3e325d12cdd6280&",
+        description: "TREATS4TROJANS, An E-ccomerce website for Gym and workout essentials launching soon.",
+        link: "https://treats4trojans.com/",
+    },
+    {
+        img: "https://cdn.builder.io/api/v1/image/assets/TEMP/49f481c2b3383cebc23245f0fd219bba135b827e98389fe9f51047c0b922eb59?apiKey=07b7466444764966b3e325d12cdd6280&",
+        description: "Candied Grasp, Hotel Landing page UI Design.",
+        link: "https://www.figma.com/design/uCpivM3fafeJpdwxlXbYdH/LANDING-PAGE?m=auto&t=ODCK7HwREn0kVoAg-6",
+    },
+    {
+        img: "https://cdn.builder.io/api/v1/image/assets/TEMP/1a92abc9bb9f34066cc8a7454083c3e5943d2e4069b9909364285d4453e8114e?apiKey=07b7466444764966b3e325d12cdd6280&",
+        description: "An ai chat bot which can answer basic questions.Disclaimer! This bot is still under development and most responses may be inaccurate.",
+        link: "https://github.com/MaxLubale/ARA.AI",
+    },
+    {
+        img: "https://images.pexels.com/photos/3747266/pexels-photo-3747266.jpeg?auto=compress&cs=tinysrgb&w=600",
+        description: "Graphic Design Work",
+        link: "https://www.canva.com/projects",
+    },
+    {
+        img: "https://images.pexels.com/photos/3584998/pexels-photo-3584998.jpeg?auto=compress&cs=tinysrgb&w=600",
+        description: "Link to a repo of Trion browser, soon to be a private and secure space for users to surf freely and ad free.",
+        link: "https://github.com/MaxLubale/Trion-browser",
+    },
+
+    {
+        img: "https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?auto=compress&cs=tinysrgb&w=600",
+        description: "This is a RESTful API built using TypeScript, NestJS framework, MongoDB, and RabbitMQ. The API provides endpoints for user management, fetching user data from an external API, and handling user avatars.",
+        link: "https://github.com/MaxLubale/payever_nestapp",
+    },
+    {
+        img: "https://images.pexels.com/photos/360591/pexels-photo-360591.jpeg?auto=compress&cs=tinysrgb&w=600",
+        description: "Link to a repo of A simple pizza restaurants app.",
+        link: "https://github.com/MaxLubale/Pizza-Restaurants-App",
+    },
+    {
+        img: "https://images.pexels.com/photos/943096/pexels-photo-943096.jpeg?auto=compress&cs=tinysrgb&w=600",
+        description: "Link to a repo of A simple web based QR Code generator .",
+        link: "https://github.com/MaxLubale/QR-code-generator",
+    },
+    {
+        img: "https://images.pexels.com/photos/276452/pexels-photo-276452.jpeg?auto=compress&cs=tinysrgb&w=600",
+        description: "A web based simple music player.",
+        link: "https://music-playerproject.netlify.app/",
+    },
+    {
+        img: "https://images.pexels.com/photos/270637/pexels-photo-270637.jpeg?auto=compress&cs=tinysrgb&w=600",
+        description: "SEO Services flyer.",
+        link: "https://www.canva.com/design/DAGLC9QtnLQ/_m-cBJo6Af1KU2IML4VlRA/edit?utm_content=DAGLC9QtnLQ&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
+    },
+    {
+        img: "https://images.pexels.com/photos/3639540/pexels-photo-3639540.jpeg?auto=compress&cs=tinysrgb&w=600",
+        description: "Real Estate Brochure",
+        link: "https://www.canva.com/design/DAGLCwZExV4/ywDH9NtKHOg7U1UCq5ZyGA/edit?utm_content=DAGLCwZExV4&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
+    },
+    {
+        img: "https://images.pexels.com/photos/5240544/pexels-photo-5240544.jpeg?auto=compress&cs=tinysrgb&w=600",
+        description: "A Presentation on command line interface.",
+        link: "https://www.canva.com/design/DAF5UEpU-I4/8t2YViCPbf6Lu96ea2OJJw/edit?utm_content=DAF5UEpU-I4&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "lubalecliff@gmail.com",
+    YOUR_FONE: "+254757039337",
+    description: "SERVICE IS CURRENTLY UNAVAILABLE!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/MaxLubale/",
+    linkedin: "https://www.linkedin.com/in/maxwell-lubale-643494280/",
+    instagram: "https://www.instagram.com/mc_lubale?igsh=eDlndTN4ajVjaHk5",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
